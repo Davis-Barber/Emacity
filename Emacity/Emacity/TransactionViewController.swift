03c34efdcb838dbd3ec075d481a00bcd3b754579
DavisBarber <dbarber.ios8@gmail.com>
2017-08-08 18:06:43 +0930

    V 0.0.1 Started work on Transactions

---
+++ Emacity/Emacity/TransactionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TransactionViewController.swift
+//  Emacity
+//
+//  Created by Davis Barber on 8/08/2017.
+//  Copyright © 2017 Davis Barber. All rights reserved.
+//
+
+import UIKit
+
+class TransactionViewController: UIViewController {
+    
+}
