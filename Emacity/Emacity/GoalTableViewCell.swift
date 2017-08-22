07a7b18c7182aab050054221647f755d1488f43d
DavisBarber <dbarber.ios8@gmail.com>
2017-08-22 13:07:35 +0930

    added goalTableViewCell.swift

---
+++ Emacity/Emacity/GoalTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  GoalTableViewCell.swift
+//  Emacity
+//
+//  Created by Davis Barber on 22/8/17.
+//  Copyright © 2017 Davis Barber. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class GoalTableViewCell: UITableViewCell {
+    
+    @IBOutlet var goalLabel: UILabel!
+    @IBOutlet var amountLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var priorityLabel: UILabel!
+    @IBOutlet var progressBar: UIProgressView!
+    @IBOutlet var percentCompleteLabel: UILabel!
+    
+    
+}
